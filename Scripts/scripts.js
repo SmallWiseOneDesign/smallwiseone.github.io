9115c0f89a1b3d79f483773b2f9d42482a6334a5
Mark Noble <github.profile@marknoble.com>
2024-11-19 23:50:40 -0500

    Wires up newsletter block to send emails requesting addition to the mailing list.

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,17 +1,23 @@
+try {
+  Typekit.load();
+} catch (e) {}
 
-      try {
-        Typekit.load();
-      } catch (e) {}
-    
+const websiteInfo = {
+  "url": "https://www.smallwiseone.com",
+  "name": "Small Wise One",
+  "description": "",
+  "@context": "http://schema.org",
+  "@type": "WebSite"
+};
 
-      {
-        "url": "https://www.smallwiseone.com",
-        "name": "Small Wise One",
-        "description": "",
-        "@context": "http://schema.org",
-        "@type": "WebSite"
-      }
-    
-
-      Static.COOKIE_BANNER_CAPABLE = true;
-    
+// Define Static as an object
+const Static = {};
+Static.COOKIE_BANNER_CAPABLE = true;
+                    
+function openEmailWindow(event) {
+  event.preventDefault();
+  const email = document.getElementById('email-yui_3_17_2_1_1552579698432_3881-field').value;
+  const subject = 'Newsletter Subscription';
+  const body = `Hello, please subscribe me at (${email}) to your newsletter, thanks!`;
+  window.location.href = `mailto:hello+newsletter.signup@smallwiseone.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+}
